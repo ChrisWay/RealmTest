a413b1d1b79e5617cc8eb507eb0bf409b6842bd8
Chris Way <chris@darksquare.net>
2017-06-06 17:15:21 +0100

    Show issue with Realm not providing the correct property name

--- RealmTest/RealmTest/MainPage.xaml.cs
+++ RealmTest/RealmTest/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Realms;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,28 @@ namespace RealmTest
     public partial class MainPage : ContentPage
     {
         public MainPage() {
+            var realm = Realm.GetInstance();
+            var ship = new Ship { Captain = new Captain() };
+            BindingContext = ship;
+            realm.Write(() =>
+                realm.Add(ship)
+            );
             InitializeComponent();
         }
+    }
+
+    public class Ship : RealmObject
+    {
+        public Captain Captain { get; set; }
+    }
+
+    public class Captain : RealmObject
+    {
+        protected override void OnPropertyChanged(string propertyName) => Debug.WriteLine(propertyName);
+        public string Name { get; set; }
+
+        [Backlink(nameof(Ship.Captain))]
+        public IQueryable<Ship> Ships { get; }
+        public string Age { get; set; }
     }
 }
